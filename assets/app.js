e5f077ff11f7e860fc91a9b55a722baa0b44e74e
David Quintana <dquintana1@gmail.com>
2018-11-07 11:56:17 -0800

    Update dev dep from uglify to uglify-es for es6 and added js to ajax the snippets to the index.html.

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,38 @@
-alert('it works');
+
+
+// wait for the document to load and be ready to handle js
+window.onload = function() {
+  getHTML();
+};
+
+// Using xmlhttprequest to pull in the html snippet
+// would rather build a react app/component but this is 
+// almost the same thing that react is doing.
+function getHTML() {
+  let tag, file;
+  // get all tags and see if the element has my custom att
+  // of x-include-html and then load in the response to innerHTML
+  tag = document.getElementsByTagName('*');
+
+  for (let i = 0; i < tag.length; i++) {
+    let item = tag[i];
+    file = item.getAttribute('x-include-html');
+    if (file) runRequest(item, file);
+  }
+}
+
+function runRequest(item, file) {
+  let  req = new XMLHttpRequest(file);
+  req.onreadystatechange = processHTML;
+  req.open('GET', file, true);
+  req.send();
+
+  function processHTML() {
+    if (this.readyState == 4) {
+      if (this.status == 200) { item.innerHTML = this.responseText; }
+      if (this.status == 404) { item.innerHTML = 'Page not found.'; }
+      item.removeAttribute('x-include-html');
+    }
+  }
+
+}
